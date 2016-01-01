96568a42088880ef8934ba67612968eb6e6fda4c
Darrell Wright <dwright-git@dawdevel.ca>
2016-01-01 16:38:13 -0500

    Added code files, oops

---
+++ parse_action.cpp
@@ -0,0 +1,50 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014-2015 Darrell Wright
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <boost/algorithm/string.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+#include "parse_action.h"
+
+
+
+parse_action_t::parse_action_t( std::vector<std::pair<std::string const, parse_action_t::action_item_t>> Actions ): actions( std::begin( Actions ), std::end( Actions ) ), separators( "\t " ) { }
+parse_action_t::parse_action_t( std::string Separators, std::vector<std::pair<std::string const, parse_action_t::action_item_t>> Actions ): actions( std::begin( Actions ), std::end( Actions ) ), separators( std::move( separators ) ) { }
+
+void parse_action_t::parse( boost::string_ref str ) const {
+	std::string tokens;
+	boost::split( tokens, str, boost::is_any_of( separators ) );
+	auto action_it = actions.find( tokens[0] );
+	if( actions.end( ) == action_it ) {
+		return;		// TODO define error action
+	}
+	auto const & action = actions->second;
+	if( std::get<0>( action ) ) {
+		tokens.erase( 0 );
+		std::get<1>( action )(tokens)
+	} else {
+		std::get<1>( action )(str.substr( tokens[0] + 1 ));
+	}
+}
---
+++ parse_action.h
@@ -0,0 +1,39 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014-2015 Darrell Wright
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <boost/utility/string_ref.hpp>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+struct parse_action_t {
+	using action_item_t = std::tuple<bool, std::function<void( std::vector<std::string> )>>;
+	std::unordered_map<std::string, action_item_t> actions;
+	std::string separators;
+
+	parse_action_t( std::vector<std::pair<std::string const, action_item_t>> Actions );
+	parse_action_t( std::string Separators, std::vector<std::pair<std::string const, action_item_t>> Actions );
+
+	void parse( boost::string_ref str ) const;
+};	// struct parse_action_t
